c39f65e77f51dfdb34ad919b8db24e73cb9153ad
Don McLean <mclean@utoronto.ca>
2022-02-06 13:53:52 -0500

    updates and file directory changes for html and assets css and js paths

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,16 +3,16 @@
 var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + ",ca&units=metric&APPID=" + key;
 var key = "76d9b9b69f8abf37f5df04749953bfe4";
 var searchHistoryId = 0;
-var cityName = ["Toronto", "CA"];
+var cityName = [[0]="Toronto", "CA"];
 
 
-// a couple of inline fetches to check api operability -- these work (although data not yet sorted and sent to page)
+/* a couple of inline fetches to check api operability -- these work (although data not yet sorted and sent to page)
 "https://api.openweathermap.org/data/2.5/weather?q=Ottawa&appid=76d9b9b69f8abf37f5df04749953bfe4";
 "https://api.openweathermap.org/data/2.5/forecast?q=Toronto&appid=76d9b9b69f8abf37f5df04749953bfe4";
-
+*/
 // function to get weather data for current search -- the url format does not work
 function getData(cityName) {
- var url = "https://api.openweathermap.org/data/2.5/weather?q=" cityName[0] + "," + cityName[1] + "&units=metric&APPID=" + key;
+ var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName[0] + "&units=metric&APPID=76d9b9b69f8abf37f5df04749953bfe4";
  fetch(url).then(function(response) {
    if (response.ok) {
     response.json().then(function(data) {
