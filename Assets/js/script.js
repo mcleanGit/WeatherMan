ce6774959c4460a94d1a6e8310d2d77ce4ab28f2
Don McLean <mclean@utoronto.ca>
2022-02-06 22:05:36 -0500

    suspended work due to time constraints

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,17 +1,56 @@
 // js for WeatherMan rev 2022-02-06
 // Open Weather API key; global variables
-var url = "https://api.openweathermap.org/data/2.5/weather?q=&APPID=76d9b9b69f8abf37f5df04749953bfe4";
+var url = "https://api.openweathermap.org/data/2.5/weather?q=" +cityName + "&units=metric&APPID=76d9b9b69f8abf37f5df04749953bfe4";
 var key = "76d9b9b69f8abf37f5df04749953bfe4";
 var searchHistoryId = 0;
 var cityName = [[0]="Toronto", "CA"];
 
-api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+// create elements for main weather section //
+var col = document.createElement("div");
+var card = document.createElement("div");
+var cardBody = document.createElement("div");
+var cardTitle = document.createElement("h5");
+var weatherIcon = document.createElement("img");
+var tempEl = document.createElement("p");
+var windEl = document.createElement("p");
+var humidityEl = document.createElement("p");
+
+col.append(card);
+card.append(cardBody);
+cardBody.append(cardTitle, weatherIcon, tempEl, windEl, humidityEl);
+
+col.setAttribute("class", "col-md");
+col.classList.add("five-day-card");
+card.setAttribute("class", "card bg-primary h-100 text-white");
+cardBody.setAttribute("class", "card-body p-2");
+cardTitle.setAttribute("class", "card-title");
+tempEl.setAttribute("class", "card-text");
+windEl.setAttribute("class", "card-text");
+humidityEl.setAttribute("class", "card-text");
+
+/* content */
+cardTitle.textContent = dayjs.unix(unixts).tz(timezone).format("dddd, MMMM, Do, YYYY, HH:mm");
+weatherIcon.setAttribute("src", iconUrl);
+weatherIcon.setAttribute("alt", iconDescription);
+tempEl.textContent = `Temp: ${tempC} °C`;
+windEl.textContent = `Wind: ${windKph} KPH`;
+humidityEl.textContent = `Humidity: ${humidity} %`;
+
+forecastContainer.append(col);
+/* top of function here ? */
+
+
+https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 /* a couple of inline fetches to check api operability -- these work (although data not yet sorted and sent to page)
 "https://api.openweathermap.org/data/2.5/weather?q=Ottawa&appid=76d9b9b69f8abf37f5df04749953bfe4";
 "https://api.openweathermap.org/data/2.5/forecast?q=Toronto&appid=76d9b9b69f8abf37f5df04749953bfe4";
 */
 /*alternative fetch structure here */
 /* fetch api data for lon and lat */
+
+function fetchWeatherInfo() {
+
+var url ="https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=76d9b9b69f8abf37f5df04749953bfe4";
 
 fetch (url)
   .then(function (response) {
@@ -23,24 +62,61 @@ fetch (url)
   })
 
   // fetch API data for 5-day forecast //
-  fetch ("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat +"&lon" + lon&appid=76d9b9b69f8abf37f5df04749953bfe4;
-// function to get weather data for current search -- this url format not working
-function getData(cityName) {
- var url = "https://api.openweathermap.org/data/2.5/weather?q=London,UK&units=metric&appid=76d9b9b69f8abf37f5df04749953bfe4";
- fetch(url).then(function(response) {
-   if (response.ok) {
-    response.json().then(function(data) {d
-     showCityData(data);
-// may be part of a subsequent fetch
-      cityName = city;
-      console.log(data);
-      showWeatherData(data);
-      forecastData(data);
-      updateSearchHistory(); 
-// does not retrieve UVI data, separate fetch needed
-    });
-    } else {
-   alert("Error: " + response.statusText);
-    }
-   });   
+  fetch ("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=76d9b9b69f8abf37f5df04749953bfe4");
+    .then(function (response) {
+      return response.json()    
+    })
+    .then(function (data) {
+      currentForecast(data);
+      fiveDayForecast(data.daily);
+    })
+
+}
+
+fetchWeatherInfo();
+
+// draft version: not useful until data retrieval is fixed.
+// scriptLS.js function to load saved data from localStorage.
+function loadSearchHistory() {
+  var size = JSON.parse(localStorage.getItem("size"));
+  for (var i = 0; i < size; i++) {
+   var key = "search" +i;
+   var text = JSON.parse(localStorage.getItem(key));
+   var historyBtn = $"<button>";
+   historyBtn.addClass("col-12 my-2 btn btn-secondary");
+   historyBtn.attr("id", key);
+   historyBtn.text(text);
+   $(".searchHistory").append(historyBtn);
+   searchHistoryId++;
+  }
  }
+ 
+ loadSearchHistory();
+ 
+ // update search history with new entry, using 8 entry max
+ function updateSearchHistory() {
+  var historyBtn = $("<button>");
+  var text = $("#searchInfo").val();
+  historyBtn.addClass("col-12 my-2 btn.secondary");
+   if (searchHistoryId = 7) {
+    $("#search0").remove();
+    for (var i = 0; i < 8; i++) {
+     $("#search" + (i+1)).attr("id", "search" +i);
+     saveSearchHistory(idvalue, text);
+    }
+    searchHistoryId = 7;
+ 
+    idvalue = "search" + searchHistoryId
+    historyBtn.attr("id", idvalue);
+    historyBtn.text(text);
+    $(".searchHistory").append(historyBtn);
+    searchHistoryId++;
+    saveSearchHistory(idvalue, text);
+   }
+ }
+ 
+ function saveSearchHistory(id, text) {
+  localStorage.setItem(id, JSON.stringify(text));
+  localStorage.setItem("size", JSON.stringify(searchHistoryId)); 
+ }
+
